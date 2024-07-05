18b2bec91cc5e0f248876e3d287761af8486a45d
dev-tpghks23 <tpghksgit23@gmail.com>
2024-07-05 21:23:12 +0900

    최초 리액트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const getDatas = async () => {
+      const response = await fetch("https://port-0-deploy-test-back-rm6l2llw1nwns6.sel5.cloudtype.app/test", {
+          method : "POST"
+      });
+      const datas = await response.json();
+      return datas;
+    }
+    
+    getDatas().then(console.log).catch(console.error);
+
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
